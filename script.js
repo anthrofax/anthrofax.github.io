25672fb3a7a9b0cc6a2b28fffa1c78d865e33679
anthrofax <afridhoikhsan@gmail.com>
2022-08-17 10:08:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function clickAnthrofax() {
+    location.replace("file:///Users/macbookair/Desktop/PERSONAL%20PORTFOLIO/index.html#home");
+}
+
+function clickViewMore() {
+    location.replace("file:///Users/macbookair/Desktop/PERSONAL%20PORTFOLIO/index.html#about");
+}
