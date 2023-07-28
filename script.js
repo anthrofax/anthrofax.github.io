ce1adabd95947c9b853f0154e01a004e230846b4
anthrofax <afridhoikhsan@gmail.com>
2023-07-28 15:01:23 +0700

    Update setelah sekian lama, belajar GITHUB lagi

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Function klik button pada Home
 function clickViewMore() {
-    location.replace("#about");
+  location.replace("#about");
 }
 // Aksi yang diberikan setelah klik tombol submit pada form contact
 const kolomNama = document.getElementById("nama");
 const tombolSubmit = document.querySelector(".contact button");
-tombolSubmit.addEventListener("click", function() {
-    alert(`Okayy ${kolomNama.value}, pesan lo udah gue terima, thank you yaa :D`);
+tombolSubmit.addEventListener("click", function () {
+  alert(`Okayy ${kolomNama.value}, pesan lo udah gue terima, thank you yaa :D`);
 });
 
 // Aksi pada tombol hamburger pada Device Mobile saat di-klik
@@ -14,24 +14,24 @@ const tombolHamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 tombolHamburger.addEventListener("click", () => {
-    tombolHamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  tombolHamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) => {
-    n.addEventListener("click", () => {
-        tombolHamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    });
+  n.addEventListener("click", () => {
+    tombolHamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
 });
 
 // Shadow pada judul 'ANTHROFAX berganti"
 const anthrofax = document.querySelector("a.nav-branding");
 const myImage = document.querySelector(".home img");
 
-setInterval(function() {
-    const r = Math.round(Math.random() * 256);
-    const g = Math.round(Math.random() * 256);
-    const b = Math.round(Math.random() * 256);
-    anthrofax.style.textShadow = `0 0 10px rgb(${r}, ${g}, ${b}), 0 0 20px rgb(${r}, ${g}, ${b}),0 0 30px rgb(${r}, ${g}, ${b})`;
+setInterval(function () {
+  const r = Math.round(Math.random() * 256);
+  const g = Math.round(Math.random() * 256);
+  const b = Math.round(Math.random() * 256);
+  anthrofax.style.textShadow = `0 0 10px rgb(${r}, ${g}, ${b}), 0 0 20px rgb(${r}, ${g}, ${b}),0 0 30px rgb(${r}, ${g}, ${b})`;
 }, 1000);
